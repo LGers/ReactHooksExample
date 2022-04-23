f0e2033ed0a498f33f7c8f221cc343a6c9c70434
LGers <87089817+LGers@users.noreply.github.com>
2022-04-23 14:18:13 +0300

    useState() hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { TodoList } from './TodoList';
 
 const App = () => {
-  const state = {
+  const initialState = {
     todos: [
-      { id: 1, title: 'First Todo', completed: true },
-      { id: 2, title: 'Second Todo', completed: false },
-    ]
+      { id: 0, title: 'First1 Todo', completed: true },
+      { id: 1, title: 'Second Todo', completed: false },
+    ],
+    title: '',
+  };
+
+  const [state, setState] = useState(initialState);
+  const onChange = (event) =>{
+    setState((prev) => ({ ...prev, title: event.target.value }))
+  };
+
+  const addTodo = (event) =>{
+    if (event.key === 'Enter') {
+      const newTodo = { id: state.todos.length, title: state.title, completed: false };
+      setState((prev) => (
+        { ...prev, todos: [ ...state.todos, newTodo], title: '' }
+        )
+      )
+    }
   };
 
   return (
     <div className="container">
       <h1>Todo App</h1>
 
+      <label>Todo name</label>
       <div className={"input-field"}>
-        <input type="text" />
-        <label>Todo name</label>
+        <input
+          type="text"
+          value={state.title}
+          onChange={onChange}
+          onKeyPress={addTodo}
+          // placeholder={'Todo name'}
+        />
       </div>
 
       <TodoList todos={state.todos} />
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const TodoItem = ({ id, title, completed }) => {
+  const [checked, setChecked] = useState(completed);
+  const cls = ['todo']
+
+  if (checked) {
+    cls.push('completed')
+  }
+
   return (
-    <li>
+    <li className={cls.join(' ')}>
       <label>
-        <input type={"checkbox"} defaultChecked={false}/>
+        <input
+          type={"checkbox"}
+          checked={checked}
+          onClick={() => setChecked(!checked)}
+        />
         <span>{title}</span>
 
-        <i className="material-icons red-text">Delete</i>
+        <i
+          className="material-icons red-text"
+        >
+          delete
+        </i>
       </label>
     </li>
   );
